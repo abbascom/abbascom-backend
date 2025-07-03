fd14ead964e0a85dd98cf65efaa9b3616087ca47
abbascom <accounts@xeriyab.com>
2025-07-03 16:45:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
 
 dotenv.config();
 

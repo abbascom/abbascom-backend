6aeab78560c1612b3cced918184fe457bc76d97e
abbascom <accounts@xeriyab.com>
2025-07-03 15:48:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// Express server setup
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Abbascom API is running");
+});
+
+// Add more routes here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
